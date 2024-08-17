b91008cfc577063326df7def63be746887cde61c
exlgame <164381172+exlgame@users.noreply.github.com>
2024-08-17 23:58:23 +0200

    Jump Controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
-
+    public bool isJumping = false;
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
+    public float jumpforce;
 
     void Start()
     {
@@ -16,6 +17,11 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
+        if(Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
         MovePlayer(horizontalMovement);
     }
 
@@ -23,6 +29,11 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 targerVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targerVelocity, ref velocity, 0.05f);
-    }
 
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpforce));
+            isJumping = false;
+        }
+    }
 }
